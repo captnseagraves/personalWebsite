4af348aba4e4485d813a605cd8eeb42735d8bef4
Kevin Seagraves <Kevin Seagraves>
2017-02-01 11:57:44 -0700

    Initial commit of personal_website to personalWebsite repo for pre-course_work

---
+++ personal_website/main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+console.log("wired up");
+
+$('#titleAnimate').addClass('animated fadeInLeft');
+$('#titleBy').addClass('animated fadeInDown');
+$('#titleName').addClass('animated fadeInRight');
+$('#titleBtn').addClass('animated fadeInUp');
+
+
+
+
+})
